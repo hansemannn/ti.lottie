bc6379e1ce99280f079dc05680d4564b5e04732f
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-02-11 02:03:20 +0100

    Update TiLottieVectorView.m

--- iphone/Classes/TiLottieVectorView.m
+++ iphone/Classes/TiLottieVectorView.m
@@ -20,14 +20,14 @@
         
         animationView = [LOTAnimationView animationNamed:resource];
         
-        if ([mode isEqual: @"aspectFit"]) {
+        if ([mode isEqualToString:@"aspectFit"]) {
             animationView.contentMode = UIViewContentModeScaleAspectFit;
-        } else if ([mode isEqual: @"aspectFill"]) {
+        } else if ([mode isEqualToString:@"aspectFill"]) {
             animationView.contentMode = UIViewContentModeScaleAspectFill;
-        } else if ([mode isEqual: @"scaleFill"]) {
+        } else if ([mode isEqualToString:@"scaleFill"]) {
             animationView.contentMode = UIViewContentModeScaleToFill;
         } else {
-            TiLogMessage(@"contentMode not valid (aspectFit, aspectFill or scaleFill), defaulting to aspectFit");
+            NSLog(@"[ERROR] The \"contentMode\" property is not valid (one of: CONTENT_MODE_ASPECT_FIT, CONTENT_MODE_ASPECT_FILL or CONTENT_MODE_SCALE_FILL), defaulting to CONTENT_MODE_ASPECT_FIT");
             animationView.contentMode = UIViewContentModeScaleAspectFit;
         }
         
